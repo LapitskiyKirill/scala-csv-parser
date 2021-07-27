b4a79693653029ba498a6f3d0f41ae41d6b857d2
kirill <kirill.lapitsky@gmail.com>
2021-07-27 15:46:55 +0300

    code refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -26,7 +26,7 @@ object Main {
     val usageFilename = config.getString("filename.usage")
     val reader = new ParameterizedReader[DriveInfo](DriveValidator, DriveMapper)
     val reporter = Reporter(path, bikeFilename, generalFilename, usageFilename)
-    val reportsMonad = processFile(reader, reporter, sourceFilenamesListWithPath)
+    val reportsMonad = processAll(reader, reporter, sourceFilenamesListWithPath)
     val a = reportsMonad.map(reports => Writer.write(reports))
     Await.ready(a, Duration.Inf)
   }
@@ -38,40 +38,117 @@ object Main {
     print((end - start) / 1000000)
   }
 
-  def processFile(reader: ParameterizedReader[DriveInfo], reporter: Reporter, sourceFilenamesListWithPath: List[String]): Future[List[Report]] = {
+  //  def processFile(reader: ParameterizedReader[DriveInfo], reporter: Reporter, sourceFilenamesListWithPath: List[String]): Future[List[Report]] = {
+  //    val bikeStatsReportGenerator = new BikeStatsReportGenerator(reporter.directoryPath, reporter.bikeStatsFilename)
+  //    val usageStatsReportGenerator = new UsageStatsReportGenerator(reporter.directoryPath, reporter.usageStatsFilename)
+  //    val generalStatsReportGenerator = new GeneralStatsReportGenerator(DateRange("2010-09-20 12:26:08", "2010-10-26 12:26:08"), reporter.directoryPath, reporter.generalStatsFilename)
+  //    val result: Future[List[Report]] = {
+  //      val reports: Future[Seq[(List[(String, Int, Int)], Array[(String, Int)], (Int, Int, Int, Int, Int))]] = Future.sequence(sourceFilenamesListWithPath.par.map(
+  //        sourceFilenameListWithPath => Future {
+  //          val lines = reader.readFile(sourceFilenameListWithPath)
+  //          (bikeStatsReportGenerator.generate(lines), usageStatsReportGenerator.generate(lines), generalStatsReportGenerator.generate(lines))
+  //        }).seq)
+  //      val bikeStats = reports.map(_.flatMap(_._1).groupBy(_._1).map(map => (map._1, map._2.map(_._2).sum, map._2.map(_._3).sum)).toList)
+  //      val usageStats = reports.map(_.flatMap(_._2).groupBy(_._1).map(map => (map._1, map._2.map(_._2).sum)).toArray)
+  //      val generalStatsFuture = reports.map(_.map(_._3))
+  //      val generalStatsList = Future.sequence(
+  //        List(
+  //          generalStatsFuture.map(_.map(_._1).sum),
+  //          generalStatsFuture.map(_.map(_._2).sum),
+  //          generalStatsFuture.map(_.map(_._3).sum),
+  //          generalStatsFuture.map(_.map(_._4).sum),
+  //          generalStatsFuture.map(_.map(_._5).sum)
+  //        )
+  //      )
+  //      val generalStats = generalStatsList.map(int => {
+  //        val iterator = int.iterator
+  //        (iterator.next(), iterator.next(), iterator.next(), iterator.next(), iterator.next())
+  //      })
+  //      Future.sequence(
+  //        List(
+  //          bikeStats.map(bikeStatsReportGenerator.generateReport),
+  //          usageStats.map(usageStatsReportGenerator.generateReport),
+  //          generalStats.map(generalStatsReportGenerator.generateReport)
+  //        )
+  //      )
+  //    }
+  //    result
+  //  }
+
+  def processAll(reader: ParameterizedReader[DriveInfo], reporter: Reporter, sourceFilenamesListWithPath: List[String]): Future[List[Report]] = {
     val bikeStatsReportGenerator = new BikeStatsReportGenerator(reporter.directoryPath, reporter.bikeStatsFilename)
     val usageStatsReportGenerator = new UsageStatsReportGenerator(reporter.directoryPath, reporter.usageStatsFilename)
     val generalStatsReportGenerator = new GeneralStatsReportGenerator(DateRange("2010-09-20 12:26:08", "2010-10-26 12:26:08"), reporter.directoryPath, reporter.generalStatsFilename)
-    val result: Future[List[Report]] = {
-      val reports: Future[Seq[(List[(String, Int, Int)], Array[(String, Int)], (Int, Int, Int, Int, Int))]] = Future.sequence(sourceFilenamesListWithPath.par.map(
-        sourceFilenameListWithPath => Future {
-          val lines = reader.readFile(sourceFilenameListWithPath)
-          (bikeStatsReportGenerator.generate(lines), usageStatsReportGenerator.generate(lines), generalStatsReportGenerator.generate(lines))
-        }).seq)
-      val bikeStats = reports.map(_.flatMap(_._1).groupBy(_._1).map(map => (map._1, map._2.map(_._2).sum, map._2.map(_._3).sum)).toList)
-      val usageStats = reports.map(_.flatMap(_._2).groupBy(_._1).map(map => (map._1, map._2.map(_._2).sum)).toArray)
-      val generalStatsFuture = reports.map(_.map(_._3))
-      val generalStatsList = Future.sequence(
-        List(
-          generalStatsFuture.map(_.map(_._1).sum),
-          generalStatsFuture.map(_.map(_._2).sum),
-          generalStatsFuture.map(_.map(_._3).sum),
-          generalStatsFuture.map(_.map(_._4).sum),
-          generalStatsFuture.map(_.map(_._5).sum)
-        )
+    //for each file-> process
+    val reports = Future.sequence(sourceFilenamesListWithPath.par.map(
+      sourceFilenameListWithPath =>
+        process(bikeStatsReportGenerator,
+          usageStatsReportGenerator,
+          generalStatsReportGenerator,
+          reader,
+          sourceFilenameListWithPath)
+    ).seq)
+    //merge all reports
+    merge(bikeStatsReportGenerator, usageStatsReportGenerator, generalStatsReportGenerator, reports)
+  }
+
+  def process(bikeStatsReportGenerator: BikeStatsReportGenerator,
+              usageStatsReportGenerator: UsageStatsReportGenerator,
+              generalStatsReportGenerator: GeneralStatsReportGenerator,
+              reader: ParameterizedReader[DriveInfo],
+              sourceFilenameListWithPath: String): Future[(List[(String, Int, Int)], Array[(String, Int)], (Int, Int, Int, Int, Int))] = {
+    //read all data from file
+    val lines = Future(read(reader, sourceFilenameListWithPath))
+    //get reports
+    lines.map(list => getReport(bikeStatsReportGenerator,
+      usageStatsReportGenerator,
+      generalStatsReportGenerator,
+      list))
+  }
+
+  //read
+  def read(reader: ParameterizedReader[DriveInfo], sourceFilenameListWithPath: String): List[Option[DriveInfo]] = {
+    reader.readFile(sourceFilenameListWithPath)
+  }
+
+  //get reports
+  def getReport(bikeStatsReportGenerator: BikeStatsReportGenerator,
+                usageStatsReportGenerator: UsageStatsReportGenerator,
+                generalStatsReportGenerator: GeneralStatsReportGenerator,
+                lines: List[Option[DriveInfo]]): (List[(String, Int, Int)], Array[(String, Int)], (Int, Int, Int, Int, Int)) = {
+    (bikeStatsReportGenerator.generate(lines), usageStatsReportGenerator.generate(lines), generalStatsReportGenerator.generate(lines))
+  }
+
+  //merge reports
+  def merge(bikeStatsReportGenerator: BikeStatsReportGenerator,
+            usageStatsReportGenerator: UsageStatsReportGenerator,
+            generalStatsReportGenerator: GeneralStatsReportGenerator,
+            reports: Future[Seq[(List[(String, Int, Int)], Array[(String, Int)], (Int, Int, Int, Int, Int))]]): Future[List[Report]] = {
+    //merge collections
+    val bikeStats = reports.map(_.flatMap(_._1).groupBy(_._1).map(map => (map._1, map._2.map(_._2).sum, map._2.map(_._3).sum)).toList)
+    val usageStats = reports.map(_.flatMap(_._2).groupBy(_._1).map(map => (map._1, map._2.map(_._2).sum)).toArray)
+    val generalStatsFuture = reports.map(_.map(_._3))
+    //merge general data
+    val generalStatsList = Future.sequence(
+      List(
+        generalStatsFuture.map(_.map(_._1).sum),
+        generalStatsFuture.map(_.map(_._2).sum),
+        generalStatsFuture.map(_.map(_._3).sum),
+        generalStatsFuture.map(_.map(_._4).sum),
+        generalStatsFuture.map(_.map(_._5).sum)
       )
-      val generalStats = generalStatsList.map(int => {
-        val iterator = int.iterator
-        (iterator.next(), iterator.next(), iterator.next(), iterator.next(), iterator.next())
-      })
-      Future.sequence(
-        List(
-          bikeStats.map(bikeStatsReportGenerator.generateReport),
-          usageStats.map(usageStatsReportGenerator.generateReport),
-          generalStats.map(generalStatsReportGenerator.generateReport)
-        )
+    )
+    val generalStats = generalStatsList.map(int => {
+      val iterator = int.iterator
+      (iterator.next(), iterator.next(), iterator.next(), iterator.next(), iterator.next())
+    })
+    //
+    Future.sequence(
+      List(
+        bikeStats.map(bikeStatsReportGenerator.generateReport),
+        usageStats.map(usageStatsReportGenerator.generateReport),
+        generalStats.map(generalStatsReportGenerator.generateReport)
       )
-    }
-    result
+    )
   }
 }
